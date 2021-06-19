eecf82475af4e9157c8701559ca5fe561d400ee3
Vishwajeet Pratap Singh <pratapvishwajeet47@gmail.com>
2021-06-19 23:03:43 +0530

    done

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import tachyons from 'tachyons'; 
 import App from './containers/App';
 
 ReactDOM.render(  
